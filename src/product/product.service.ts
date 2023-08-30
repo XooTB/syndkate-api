7c69c9314ea9153d4dc75a38de385da94cdec2b3
Samiul Alim <xo.ot2001@gmail.com>
2023-08-30 09:02:01 +0600

    Updated request handler

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { productSchema } from './schemas/product.schema';
@@ -8,9 +8,17 @@ export class ProductService {
   constructor(@InjectModel('Product') private Product) {}
 
   async search(query: string) {
-    return this.Product.find({
+    const products = await this.Product.find({
       productTitle: { $regex: query, $options: 'i' },
     });
+
+    if (!products.length) {
+      throw new HttpException(
+        'No Products found. Please try changing the query.',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return products;
   }
 
   async details(id: string) {
